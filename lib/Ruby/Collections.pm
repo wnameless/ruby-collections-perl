01d5c036a75993b968a17e466d95727c3e1f0f3e
WMW <wnameless@gmail.com>
2013-04-22 20:35:59 +0800

    Revert last change

--- lib/Ruby/Collections.pm
+++ lib/Ruby/Collections.pm
@@ -55,9 +55,7 @@ sub rh {
 	}
 	elsif ( @_ == 1 ) {
 		if ( reftype( $_[0] ) eq 'HASH' ) {
-			while ( my ( $key, $val ) = each %{ $_[0] } ) {
-				$new_hash->{$key} = $val;
-			}
+			%new_hash = %{ $_[0] };
 		}
 		else {
 			die 'Input is not a HASH.';
